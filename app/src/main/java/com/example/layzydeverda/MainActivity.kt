c6637161082661560490c27e932bb61bb685beff
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-01-14 22:49:09 +0100

    feat: implement main and add implementations

--- app/src/main/java/com/example/layzydeverda/MainActivity.kt
+++ app/src/main/java/com/example/layzydeverda/MainActivity.kt
@@ -4,44 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.layzydeverda.nav.EntryPoint
 import com.example.layzydeverda.ui.theme.LayzyDeVerdaTheme
+import com.example.lazy_loading.viewModel.ApiViewModel
+
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val ApiViewModel by viewModels<ApiViewModel>()
         enableEdgeToEdge()
         setContent {
             LayzyDeVerdaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navigationController = rememberNavController()
+                    EntryPoint(navigationController, ApiViewModel)
+                }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LayzyDeVerdaTheme {
-        Greeting("Android")
-    }
-}
