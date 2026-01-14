81a3e5fd815b436d6000ba6470ba7568259fb9be
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-01-14 22:45:05 +0100

    feat: implment vm

--- app/src/main/java/com/example/layzydeverda/viewModel/ApiViewModel.kt
+++ app/src/main/java/com/example/layzydeverda/viewModel/ApiViewModel.kt
@@ -1,4 +1,18 @@
-package com.example.layzydeverda.viewModel
+package com.example.lazy_loading.viewModel
 
-class ApiViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.layzydeverda.model.Country
+import com.example.layzydeverda.model.CountryList
+
+
+class ApiViewModel : ViewModel() {
+
+    private val _cList = CountryList()
+    //privat i modificable en el VM
+    private val _countries = MutableLiveData<List<Country>>(_cList.getCountries())
+    //public pero no modificable
+    val countries: LiveData<List<Country>> = _countries
+
 }
