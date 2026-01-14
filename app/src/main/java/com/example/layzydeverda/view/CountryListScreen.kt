50f18eb20c4eab53260d5dfb611556ebd32b551f
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-01-14 22:47:22 +0100

    feat: implement countries list view

--- app/src/main/java/com/example/layzydeverda/view/CountryListScreen.kt
+++ app/src/main/java/com/example/layzydeverda/view/CountryListScreen.kt
@@ -1,2 +1,33 @@
-package com.example.layzydeverda.view
+package com.example.lazy_loading.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.layzydeverda.nav.Routes
+import com.example.lazy_loading.viewModel.ApiViewModel
+
+@Composable
+fun CountryList(navController: NavController, viewModel: ApiViewModel) {
+    val countries by viewModel.countries.observeAsState(emptyList())
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(4.dp),
+        modifier = Modifier
+            .padding(vertical = 30.dp)
+            .fillMaxWidth()
+    ) {
+        items(countries) { country ->
+            CountryItem(country = country) {
+                navController.navigate(Routes.CountryDetailScreen.createRoute(country.name))
+            }
+        }
+    }
+}
