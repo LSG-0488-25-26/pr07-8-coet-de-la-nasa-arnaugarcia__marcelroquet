4a9d9583244e0ae90845aec59d143b8c14574518
Arnau Garcia <arnau.garcia@gracia.lasalle.cat>
2026-01-22 16:45:55 +0100

    Feat: getAllCountries() method

--- app/src/main/java/com/example/layzydeverda/viewModel/ApiViewModel.kt
+++ app/src/main/java/com/example/layzydeverda/viewModel/ApiViewModel.kt
@@ -1,14 +1,35 @@
-package com.example.lazy_loading.viewModel
+package com.example.layzydeverda.viewModel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.layzydeverda.model.Country
-
-
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import android.util.Log
+import com.example.layzydeverda.api.Repository
 
 class ApiViewModel : ViewModel() {
+    private val repository = Repository()
+    private val _loading = MutableLiveData(true)
+    val loading = _loading
+    private val _countryList = MutableLiveData<List<Country>>()
+    val countryList : LiveData<List<Country>> = _countryList
 
+    fun getAllCountries() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val response = repository.getCountries()
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    _countryList.value = response.body()
+                    _loading.value = false
 
-
+                } else {
+                    Log.e("Error :", response.message())
+                }
+            }
+        }
+    }
 }
