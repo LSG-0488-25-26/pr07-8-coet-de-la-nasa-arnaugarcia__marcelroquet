3abda5421b172c9d60abb224c10233b0476c4e7d
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-02-05 15:59:45 +0100

    feat: conutry item

--- app/src/main/java/com/example/layzydeverda/view/CountryItem.kt
+++ app/src/main/java/com/example/layzydeverda/view/CountryItem.kt
@@ -1,61 +1,131 @@
-package com.example.lazy_loading.view
+package com.example.layzydeverda.view
 
 import androidx.compose.material3.Card
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.DismissDirection
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.SwipeToDismiss
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.font.FontWeight
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import com.example.layzydeverda.model.Country
-@OptIn(ExperimentalGlideComposeApi::class)
-@Composable
-fun CountryItem(country: Country, onItemSelected: (String) -> Unit) {
-    Card(
-        border = BorderStroke(1.dp, androidx.compose.ui.graphics.Color.LightGray),
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable { onItemSelected(country.name.common) }
-            .padding(8.dp)
-            .height(100.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            GlideImage(
-                model = country.flags.png,
-                contentDescription = country.flags.alt,
-                modifier = Modifier.size(80.dp),
-                contentScale = ContentScale.Crop
-            )
+import com.example.layzydeverda.model.CountryEntity
 
-            Spacer(modifier = Modifier.width(16.dp))
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = country.name.common,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = MaterialTheme.typography.titleLarge.fontSize
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = "Capital: ${country.capital}",
-                    fontSize = MaterialTheme.typography.bodyLarge.fontSize
-                )
-            }
+@OptIn(
+    ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class,
+    ExperimentalMaterialApi::class
+)
+@Composable
+fun CountryItem(
+    country: CountryEntity,
+    onItemSelected: (String) -> Unit,
+    onToggleFavourite: (CountryEntity) -> Unit,
+    dismissable: (Boolean) = false // Predefinit que no es pugui
+) {
+    //https://proandroiddev.com/swipe-to-dismiss-jetpack-compose-f874c65cfd95
+    val dismissState = rememberDismissState(
+        // mai es borri realmentchi
+        confirmStateChange = { false }
+    )
+
+    LaunchedEffect(dismissState.targetValue) {
+        if (dismissState.targetValue == DismissValue.DismissedToStart) {
+            // Li passem al pare
+            onToggleFavourite(country)
+            // Resetegem on estava el country, no el borrem
+            dismissState.reset()
         }
     }
+
+    SwipeToDismiss(
+        state = dismissState,
+        // Només es pot swipear a l'esquerra
+        directions = setOf(DismissDirection.EndToStart),
+        dismissThresholds = { FractionalThreshold(0.4f) },
+        background = {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        // Fer gradient horitzontal
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(Color.LightGray, Color.DarkGray)
+                        )
+                    )
+                    .padding(8.dp),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                when {
+                    dismissable -> Icon(Icons.Filled.Delete, contentDescription = "Delete")
+                    country.isFav -> Icon(Icons.Filled.Favorite, contentDescription = "Fav")
+                    else -> Icon(Icons.Filled.FavoriteBorder, contentDescription = "Not Fav")
+                }
+            }
+        },
+        dismissContent = {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.DarkGray
+                ),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onItemSelected(country.name) }
+                    .height(70.dp),
+                shape = RectangleShape,
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp, 8.dp)
+                ) {
+                    GlideImage(
+                        model = country.flagUrl,
+                        contentDescription = country.flagUrl,
+                        modifier = Modifier
+                            .height(40.dp)
+                            .aspectRatio(1.6f),   // Aspect ratio de bandera normal 16:9
+                        contentScale = ContentScale.Crop
+                    )
+
+                    Spacer(modifier = Modifier.width(12.dp))
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = country.name,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        Text(
+                            text = "Capital: ${country.capital}",
+                            fontSize = MaterialTheme.typography.bodySmall.fontSize
+
+                        )
+                    }
+                }
+            }
+        }
+    )
 }
