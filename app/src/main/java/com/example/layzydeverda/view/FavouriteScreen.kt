20167eefa7661c5f9ac9611f3eed894af3432777
Marcel Roquet <marcel.roquet@gracia.lasalle.cat>
2026-02-08 13:21:21 +0100

    fix: veure emptyFavorites en tota la pantalla

--- app/src/main/java/com/example/layzydeverda/view/FavouriteScreen.kt
+++ app/src/main/java/com/example/layzydeverda/view/FavouriteScreen.kt
@@ -3,7 +3,9 @@ package com.example.layzydeverda.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,29 +15,36 @@ import androidx.compose.material.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.layzydeverda.nav.Routes
 import com.example.layzydeverda.viewModel.ApiViewModel
-
 @Composable
 fun FavoriteScreen(navController: NavController, viewModel: ApiViewModel) {
     val countries by viewModel.countryList.observeAsState(emptyList())
     val favoriteCountries = countries.filter { it.isFav }
 
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(4.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.DarkGray)
-    ) {
-        if (favoriteCountries.isEmpty()) {
-            item {
-                EmptyFavoritesMessage()
-            }
-        } else {
+    if (favoriteCountries.isEmpty()) {
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Black)
+        ) {
+            EmptyFavoritesMessage(
+
+            )
+        }
+    } else {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black)
+        ) {
             items(favoriteCountries) { country ->
                 CountryItem(
                     country = country,
@@ -57,4 +66,3 @@ fun FavoriteScreen(navController: NavController, viewModel: ApiViewModel) {
         }
     }
 }
-
